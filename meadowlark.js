68ede8573276d1f7f332313f9c39681fe7822bfc
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2022-11-10 09:05:50 +0700

    ch04

--- meadowlark.js
+++ meadowlark.js
@@ -1,44 +1,39 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const app = express();
-const port = process.env.PORT || 3000
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-   ]
-   
-app.use(express.static(__dirname + '/public'))
 
-// configure Handlebars view engine
+const fortune = require('./lib/fortune.js')
+
+const app = express()
+
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
-   }))
-   app.set('view engine', 'handlebars');
+  }))
+  app.set('view engine', 'handlebars')
 
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => {
-    const randomFortune =
-   fortunes[Math.floor(Math.random()*fortunes.length)]
- res.render('about', { fortune: randomFortune })
-})
 
+app.get('/about', (req, res) => {
+  res.render('about', { fortune: fortune.getFortune() })
+})
 
 // custom 404 page
-app.use( (req, res) => {
- res.status(404) 
- res.render("404");
+app.use((req, res) => {
+  res.status(404)
+  res.render('404')
 })
+
 // custom 500 page
 app.use((err, req, res, next) => {
- console.error(err.message)
- res.type('text/plain')
- res.status(500)
- res.send('500 - Server Error')
+  console.error(err.message)
+  res.status(500)
+  res.render('500')
 })
-app.listen(port, () => console.log(
- `Express started on http://localhost:${port}; ` +
- `press Ctrl-C to terminate.`))
+
+app.listen(port, () => {
+  console.log( `Express started on http://localhost:${port}` +
+    '; press Ctrl-C to terminate.' )
+})
