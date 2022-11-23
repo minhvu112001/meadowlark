f24ff68e16483b2e33ec39372a0fea2b9b96e31d
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2022-11-23 16:01:08 +0700

    all

--- register/register.js
+++ register/register.js
@@ -1,56 +1,38 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-const multiparty = require('multiparty')
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+var bodyParser = require('body-parser')
+var connect = require("connect");
 
-const handlers = require('./lib/handlers')
-const flashMiddlware = require('./lib/middleware/flash')
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const app = express()
+app.engine(
+    'handlebars',
+    expressHandlebars.engine({
+        defaultLayout: 'main',
+    })
+);
 
-// configure Handlebars view engine
-app.engine('handlebars', expressHandlebars.engine({
-  defaultLayout: 'main',
-  helpers: {
-    section: function(name, options) {
-      if(!this._sections) this._sections = {}
-      this._sections[name] = options.fn(this)
-      return null
-    },
-  },
-}))
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.get('/', (req, res) => res.render('home'));
 
-const port = process.env.PORT || 8000
+app.get('/form', (req, res) => res.render('form'));
 
-app.use(express.static(__dirname + '/public'))
-
-
-
-app.get('/', handlers.home)
-
-// handlers for browser-based form submission
-app.get('/newsletter-signup', handlers.newsletterSignup)
-app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
-app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
-
-// handlers for fetch/JSON form submission
-app.get('/newsletter', handlers.newsletter)
-app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+app.post('/thank-you', function(req, res, next) {
+    const {name, gender, birthdate, classname, registrationcode} = req.body;
+    res.render("thank-you", { name, gender,birthdate,classname,registrationcode
+    });
+    console.log("Username is "+ name)
+    console.log("Your birthdate is " + birthdate)
+    console.log("Your gender is "+ gender)
+    console.log("Your classname is " + classname)
+    console.log("Your registration code is "+ registrationcode)
+});
 
 
-
-app.use(handlers.notFound)
-app.use(handlers.serverError)
-
-if(require.main === module) {
-  app.listen(port, () => {
-    console.log( `Express started on http://localhost:${port}` +
-      '; press Ctrl-C to terminate.' )
-  })
-} else {
-  module.exports = app
-}
+app.listen(3000, () => {
+  console.log('Listening on http://localhost:3000');
+})
